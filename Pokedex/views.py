205a3daa29f4cdc3a356cd68facf8656d6e97691
mcashmore <cashmore.mackenzie@gmail.com>
2025-01-29 17:36:47 -0500

    got basic interaction with list and displaying details

--- Pokedex/views.py
+++ Pokedex/views.py
@@ -25,22 +25,18 @@ def pokemon_next(request):
     global limit
     global maxPokemonCount
     
-    
     if (offset + limit > maxPokemonCount-limit):
-        print("at limit")
         offset = maxPokemonCount - limit
+        index = limit - 1
     else:
         print("next")
         offset = offset+limit
-    print(F"condition {offset + limit > maxPokemonCount}")
-    print(f"offset {offset}")
-    print(f"limit {limit}")
-    print(f"limit and offset {offset + limit }")
+        index = 0
      
     pokemonData = ApiService.GetPokemon(limit, offset)
     pokemonList = [pokemon.to_dict() for pokemon in pokemonData]
     
-    return JsonResponse({"pokemon":pokemonList}, safe=False)
+    return JsonResponse({"pokemon":pokemonList, "index":index}, safe=False)
 
 def pokemon_previous(request):
     global offset
@@ -48,31 +44,12 @@ def pokemon_previous(request):
     global maxPokemonCount
     if offset - limit < 0:
         offset = 0
+        index = 0
     else:
         offset = offset - limit
+        index = limit - 1
         
     pokemonData = ApiService.GetPokemon(limit, offset)
     pokemonList = [pokemon.to_dict() for pokemon in pokemonData]
     
-    return JsonResponse({"pokemon":pokemonList}, safe=False)
-    
-# def index(request):
-    
-#     pokemonList = ApiService.GetPokemon()
-#     print(pokemonList)
-   
-#     return render(request, "pokedex/index.html",{"poke":poke, "pokemonlist":pokemonList, })
-
-
-# def getPokemons(request):
-#     pokemonlist = ApiService.GetPokemons()
-    
-#     pokemonData = [pokemon.to_dict() for pokemon in pokemonlist]
-    
-#     return JsonResponse({"newList":pokemonData},safe=False)
-
-# def getPokemonbyUrl(request, name):
-#     print("At url")
-#     poke = ApiService.getPokemonByUrl(name)
-    
-#     return JsonResponse({"pokemon":poke.to_dic()},safe=False)
+    return JsonResponse({"pokemon":pokemonList, "index":index}, safe=False)
