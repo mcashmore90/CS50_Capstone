27f158c0b7022b4f42c4984eff0ed35ac1442a71
mcashmore <cashmore.mackenzie@gmail.com>
2025-02-05 12:24:06 -0500

    fresh pull

---
+++ Pokedex/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect,JsonResponse
+from .services.api_service import ApiService
+import json
+
+offset = 0
+limit = 6
+maxPokemonCount = 151
+
+def index(request):
+    return render(request, "pokedex/index.html")
+
+def pokemon(request):
+    pokemonData = ApiService.GetPokemon(limit, offset)
+    pokemonList = [pokemon.to_dict() for pokemon in pokemonData]
+    return JsonResponse({"pokemon":pokemonList}, safe=False)
+
+def pokemon_details(request, name):
+    pokemon = ApiService.GetPokemonByName(name)
+    return JsonResponse({"pokemon":pokemon.to_dic()},safe=False)
+
+def pokemon_next(request):
+    global offset
+    global limit
+    global maxPokemonCount
+    
+    if (offset + limit > maxPokemonCount-limit):
+        offset = maxPokemonCount - limit
+        index = limit - 1
+    else:
+        print("next")
+        offset = offset+limit
+        index = 0
+     
+    pokemonData = ApiService.GetPokemon(limit, offset)
+    pokemonList = [pokemon.to_dict() for pokemon in pokemonData]
+    
+    return JsonResponse({"pokemon":pokemonList, "index":index}, safe=False)
+
+def pokemon_previous(request):
+    global offset
+    global limit
+    global maxPokemonCount
+    if offset - limit < 0:
+        offset = 0
+        index = 0
+    else:
+        offset = offset - limit
+        index = limit - 1
+        
+    pokemonData = ApiService.GetPokemon(limit, offset)
+    pokemonList = [pokemon.to_dict() for pokemon in pokemonData]
+    
+    return JsonResponse({"pokemon":pokemonList, "index":index}, safe=False)
---
+++ README.md
@@ -0,0 +1,29 @@
+Capstone project for HarvardX CS50’s Web Programming with Python and JavaScript course using https://pokeapi.co/ API to recreate the Pokedex from Pokemon generation 1.
+
+Postman APIs to be used:
+https://www.postman.com/navigation-observer-99340780/pokemon-api/collection/miirbob/apis?action=share&creator=38650772
+
+
+This project will be completed in multiple phases:
+
+Phase 1: Project Setup --COMPLETE--
+  Initial setup of the project and creation of the repository.
+
+Phase 2: API Service Setup
+  Create models to be used to return data from api call
+  Initial creation and testing of APIs and return data to the front end.
+
+Phase 3: UI Testing/Setup
+  Testing creating certain objects for the UI with CSS. Initial layout of UI.
+
+Phase 4: Interaction
+  Create scripts to interact with the UI with base data retrieval 
+
+Phase 5: Data Retrieval 
+  Create full functional API to get all required data and display on the UI
+
+Phase 5.A: Basic Layout
+  layout foundation for the webpage 
+
+Phase 6: Final Process
+  Complete all required UI and functionality for project.
